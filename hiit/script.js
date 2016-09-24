01bb86833e9bf27a8e5260c7884e8ac9e6630bd2
KaiUsher <kai.usher97@gmail.com>
2016-09-24 15:55:06 +0100

    Validation
    -Validate inputs
    -Changed red colour
    -Upped END font size

--- hiit/script.js
+++ hiit/script.js
@@ -1,6 +1,46 @@
 var repCountTracker = 0;
 var setCountTracker = 1;
 
+function validateInputs(){
+	
+	//Initialise check variables as false to ensure checks are made.
+	var workTimeCheck = false;
+	var restTimeCheck = false;
+	var setsCheck = false;
+	var repsCheck = false;
+	
+	//Pull values from the form.
+	var workTimer = document.getElementById("workout").value;
+	var restTimer = document.getElementById("rest").value;
+	var repCount = document.getElementById("reps").value;
+	var setCount = document.getElementById("sets").value;
+	
+	if((isNaN(workTimer) == false) && workTimer > 0){
+		workTimeCheck = true;
+	}
+	
+	if((isNaN(restTimer) == false) && restTimer > 0){
+		restTimeCheck = true;
+	}
+	
+	if((isNaN(setCount) == false) && setCount > 0){
+		setsCheck = true;
+	}
+	
+	if((isNaN(repCount) == false) && repCount > 0){
+		repsCheck = true;
+	}
+	
+	//Only start the timer if all inputs have been checked and are valid.
+	if((workTimeCheck == true) && (restTimeCheck == true) && (setsCheck == true) && (repsCheck == true)){
+		startTimer();
+	}else{
+		alert("There is a problem with one or more of your inputs; please ensure you are using only positive numbers!");
+	}
+	
+}
+
+
 function startTimer(){
 	
 	var workTimer = document.getElementById("workout").value;
@@ -38,7 +78,6 @@ function workTimerFunc(workSeconds,restTimer,repCount,repCountTracker,setCount,s
 	id = setInterval(function(){
 		workCounter--;
 		if(workCounter < 0){
-			//document.getElementById("timerCount").innerHTML = "END";
 			clearInterval(id);
 			restTimerFunc(restTimer,repCount,workSeconds,repCountTracker,setCount,setCountTracker)
 		} else {
@@ -54,7 +93,7 @@ function restTimerFunc(restSeconds,repCount,workSeconds,repCountTracker,setCount
 	
 	document.getElementById("timerCount").style.fontSize = "200px";
 	document.getElementById("timerCount").innerHTML = restCounter.toString();
-	document.getElementById("timerCount").style.color = 'red';
+	document.getElementById("timerCount").style.color = '#e60000';
 	document.getElementById("activity").innerHTML = "rest";
 	
 	id = setInterval(function(){
@@ -68,7 +107,7 @@ function restTimerFunc(restSeconds,repCount,workSeconds,repCountTracker,setCount
 				clearInterval(id);
 				document.getElementById("timerCount").innerHTML = "END";
 				document.getElementById("context").innerHTML = "You smashed it!";
-				document.getElementById("timerCount").style.fontSize = "120px";
+				document.getElementById("timerCount").style.fontSize = "135px";
 				document.getElementById("timerCount").style.color = '#262626';
 			} else {
 				clearInterval(id);
