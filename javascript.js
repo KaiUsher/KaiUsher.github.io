8dfb23f136971ff0d3c34aaa0a798c3a06f1bd6c
KaiUsher <kai.usher97@gmail.com>
2017-02-05 13:17:02 +0000

    Mobile link fix

--- javascript.js
+++ javascript.js
@@ -2,37 +2,38 @@ function makeVisible(choice) {
 	console.log(choice);
 	switch(choice) {
 		case "about":
-			document.getElementById("ethos").style.opacity ="0";
-			document.getElementById("skills").style.opacity ="0";
-			document.getElementById("work").style.opacity ="0";
-			document.getElementById("contact").style.opacity ="0";
+			document.getElementById("ethos").style.visibility ="hidden";
+			document.getElementById("skills").style.visibility ="hidden";
+			document.getElementById("work").style.visibility ="hidden";
+			document.getElementById("contact").style.visibility ="hidden";
 			break;
 		case "ethos":
-			document.getElementById("about").style.opacity ="0";
-			document.getElementById("skills").style.opacity ="0";
-			document.getElementById("work").style.opacity ="0";
-			document.getElementById("contact").style.opacity ="0";
+			document.getElementById("about").style.visibility ="hidden";
+			document.getElementById("skills").style.visibility ="hidden";
+			document.getElementById("work").style.visibility ="hidden";
+			document.getElementById("contact").style.visibility ="hidden";
 			break;
 		case "skills":
-			document.getElementById("about").style.opacity ="0";
-			document.getElementById("ethos").style.opacity ="0";
-			document.getElementById("work").style.opacity ="0";
-			document.getElementById("contact").style.opacity ="0";
+			document.getElementById("about").style.visibility ="hidden";
+			document.getElementById("ethos").style.visibility ="hidden";
+			document.getElementById("work").style.visibility ="hidden";
+			document.getElementById("contact").style.visibility ="hidden";
 			break;
 		case "work":
-			document.getElementById("about").style.opacity ="0";
-			document.getElementById("ethos").style.opacity ="0";
-			document.getElementById("skills").style.opacity ="0";
-			document.getElementById("contact").style.opacity ="0";
+			document.getElementById("about").style.visibility ="hidden";
+			document.getElementById("ethos").style.visibility ="hidden";
+			document.getElementById("skills").style.visibility ="hidden";
+			document.getElementById("contact").style.visibility ="hidden";
 			break;
 		case "contact":
-			document.getElementById("about").style.opacity ="0";
-			document.getElementById("ethos").style.opacity ="0";
-			document.getElementById("work").style.opacity ="0";
-			document.getElementById("skills").style.opacity ="0";
+			document.getElementById("about").style.visibility ="hidden";
+			document.getElementById("ethos").style.visibility ="hidden";
+			document.getElementById("work").style.visibility ="hidden";
+			document.getElementById("skills").style.visibility ="hidden";
 			break;
 		default:
 			break;
 	}
-	document.getElementById(choice).style.opacity ="1";
+	document.getElementById(choice).style.visibility ="visible";
+	document.getElementById(choice).style.zIndex ="1";
 }
